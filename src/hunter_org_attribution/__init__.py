28e44237177e0a0bf9663956843d940a7421d75a
blackcat312340 <167671118+blackcat312340@users.noreply.github.com>
2026-09-17 14:57:25 +0800

    Expose Measurement 212 production projection API

--- src/hunter_org_attribution/__init__.py
+++ src/hunter_org_attribution/__init__.py
@@ -3,14 +3,16 @@
 from .engine import AttributionEngine
 from .hunter_adapter import normalize_hunter_record
 from .models import AttributionResult, NormalizedHunterRecord
+from .production_adapter import Measurement212Projection, project_measurement212_hunter_record
 from .provenance import AuthoritySpec, load_authority
 
 __all__ = [
     "AttributionEngine",
     "AttributionResult",
     "AuthoritySpec",
+    "Measurement212Projection",
     "NormalizedHunterRecord",
     "load_authority",
     "normalize_hunter_record",
+    "project_measurement212_hunter_record",
 ]
-
